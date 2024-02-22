16d2315df402b2d4406a4d26dce1e142c8ab68e9
Hugo Vitorino <42554154+hvictorino@users.noreply.github.com>
2024-02-22 09:39:17 -0300

    Update arvore_binaria.c
    Código com funções para inserir e remover de forma balanceada, além de impressão em forma visual de árvore deitada e utilizando os percursos (inorder,preorder,posorder)

--- arvore_binaria.c
+++ arvore_binaria.c
@@ -20,7 +20,7 @@ No *novoNo(int valor){
 		novo->conteudo = valor;
 		novo->esq = NULL;
 		novo->dir = NULL;
-		novo->alura = 0;
+		novo->altura = 0;
 	}else
 		printf("\nERRO ao alocar nó em novoNo!\n");
 	
@@ -67,8 +67,8 @@ No *rotacaoEsquerda(No *raiz){
 	p->esq = raiz;
 	raiz->dir = q;
 	
-	raiz->altura = maior(alturaDoNo(raiz->esq), alturaDoNo(raiz->dir) + 1;
-	p->altura = maior(alturaDoNo(p->esq), alturaDoNo(p->dir) + 1;
+	raiz->altura = maior(alturaDoNo(raiz->esq), alturaDoNo(raiz->dir)) + 1;
+	p->altura = maior(alturaDoNo(p->esq), alturaDoNo(p->dir)) + 1;
 	
 	return p;
 }
@@ -85,8 +85,8 @@ No *rotacaoDireita(No *raiz){
 	p->dir = raiz;
 	raiz->esq = q;
 	
-	raiz->altura = maior(alturaDoNo(raiz->esq), alturaDoNo(raiz->dir) + 1;
-	p->altura = maior(alturaDoNo(p->esq), alturaDoNo(p->dir) + 1;
+	raiz->altura = maior(alturaDoNo(raiz->esq), alturaDoNo(raiz->dir)) + 1;
+	p->altura = maior(alturaDoNo(p->esq), alturaDoNo(p->dir)) + 1;
 	
 	return p;
 }
@@ -95,7 +95,7 @@ No *rotacaoDireita(No *raiz){
 	Função para a rotação direita-esquerda
 */
 No *rotacaoDireitaEsquerda(No *raiz){
-	raiz->dir = rotacaoDireita(r->dir);
+	raiz->dir = rotacaoDireita(raiz->dir);
 	return rotacaoEsquerda(raiz);
 }
 
@@ -103,35 +103,8 @@ No *rotacaoDireitaEsquerda(No *raiz){
 	Função para a rotação esquerda-direita
 */
 No *rotacaoEsquerdaDireita(No *raiz){
-	raiz->esq = rotacaoEsquerda(r->esq);
+	raiz->esq = rotacaoEsquerda(raiz->esq);
 	return rotacaoDireita(raiz);
-}
-
-/*
-	Insere um novo nó na árvore
-	raiz -> raiz da árvore
-	valor -> valor a ser inserido
-	Retorno: endereço do novo nó ou nova raiz após o balanceamento
-*/
-No *inserir(No *raiz, int valor){
-	if(raiz == NULL) // árvore vazia
-		return novoNo(valor);
-	else{
-		if(valor < raiz->valor)
-			raiz->esq = inserir(raiz->esq, valor);
-		if(valor > raiz-> valor)
-			raiz->dir = inserir(raiz->dir, valor);
-		if(valor == raiz->valor)
-			printf("\nJá existe um elemento de valor %d nesta árvore!\n", valor);
-	}
-	
-	// Recalcula a altura de todos os nós entre a raiz e o novo nó inserido
-	raiz->altura = maior(alturaDoNo(raiz->esq), alturaDoNo(raiz->dir) + 1;
-	
-	// verifica se é necessário rebalancear a árvore
-	raiz = balancear(raiz);
-	
-	return raiz;
 }
 
 /*
@@ -146,7 +119,7 @@ No *balancear(No *raiz){
 		raiz = rotacaoEsquerda(raiz);
 	
 	// Rotação à direita
-	else if(fb > 1 && fatorDeBalanceamento(raiz->esq >= 0)
+	else if(fb > 1 && fatorDeBalanceamento(raiz->esq) >= 0)
 		raiz = rotacaoDireita(raiz);
 	
 	// Rotação dupla à esquerda
@@ -161,20 +134,47 @@ No *balancear(No *raiz){
 }
 
 /*
+	Insere um novo nó na árvore
+	raiz -> raiz da árvore
+	valor -> valor a ser inserido
+	Retorno: endereço do novo nó ou nova raiz após o balanceamento
+*/
+No *inserir(No *raiz, int valor){
+	if(raiz == NULL) // árvore vazia
+		return novoNo(valor);
+	else{
+		if(valor < raiz->conteudo)
+			raiz->esq = inserir(raiz->esq, valor);
+		if(valor > raiz->conteudo)
+			raiz->dir = inserir(raiz->dir, valor);
+		if(valor == raiz->conteudo)
+			printf("\nJá existe um elemento de valor %d nesta árvore!\n", valor);
+	}
+	
+	// Recalcula a altura de todos os nós entre a raiz e o novo nó inserido
+	raiz->altura = maior(alturaDoNo(raiz->esq), alturaDoNo(raiz->dir)) + 1;
+	
+	// verifica se é necessário rebalancear a árvore
+	raiz = balancear(raiz);
+	
+	return raiz;
+}
+
+/*
 	Remove um nó da árvore
 	raiz -> raiz da árvore
 	valor -> valor a ser removido
 */
 No *remover(No *raiz, int valor){
 	if(raiz == NULL){
-		printf("Valor não encontrado\n");
+		printf("\nValor não encontrado\n");
 		return NULL;
 	}else{ // procura o nó a remover
 		if(raiz->conteudo == valor){
 			// remove nós folhas
 			if(raiz->esq == NULL && raiz->dir == NULL){
 				free(raiz);
-				printf("Elemento folha removido: %d\n",valor);
+				printf("\nElemento folha removido: %d\n",valor);
 				return NULL;
 			}else{
 				// remover nós que possuem 2 filhos
@@ -185,7 +185,7 @@ No *remover(No *raiz, int valor){
 					
 					raiz->conteudo = aux->conteudo;
 					aux->conteudo = valor;
-					printf("Elemento trocado: %d\n",valor);
+					printf("\nElemento trocado: %d\n",valor);
 					raiz->esq = remover(raiz->esq, valor);
 					return raiz;
 				}else{
@@ -197,19 +197,19 @@ No *remover(No *raiz, int valor){
 						aux = raiz->dir;
 					
 					free(raiz);
-					printf("Elemento com 1 filho removido: %d\n",valor);
+					printf("\nElemento com 1 filho removido: %d\n",valor);
 					return aux;
 				}
 			}
 		}else{
-			if(chave < raiz->conteudo)
+			if(valor < raiz->conteudo)
 				raiz->esq = remover(raiz->esq, valor);
-			if(chave > raiz->conteudo)
+			if(valor > raiz->conteudo)
 				raiz->dir = remover(raiz->dir, valor);
 		}
 		
 		// Recalcula a altura dos nós
-		raiz->altura = maior(alturaDoNo(raiz->esq), alturaDoNo(raiz->dir) + 1;
+		raiz->altura = maior(alturaDoNo(raiz->esq), alturaDoNo(raiz->dir)) + 1;
 		
 		// Faz o rebalanceamento caso seja necessário
 		raiz = balancear(raiz);
@@ -218,12 +218,101 @@ No *remover(No *raiz, int valor){
 	}
 }
 
+/*
+	Função para imprimir a árvore no console
+*/
+void imprimir(No *raiz, int nivel){
+	int i;
+	if(raiz){
+		imprimir(raiz->dir, nivel + 1);
+		printf("\n\n");
+		
+		for(i = 0; i < nivel; i++)
+			printf("\t");
+		
+		printf("%d",raiz->conteudo);
+		imprimir(raiz->esq, nivel + 1);
+	}
+}
+
+
+/*
+	Função para imprimir a árvore com base nos percursos
+	Em Ordem
+	Pré-ordem
+	Pós-ordem
+*/
+void imprimirPercurso(No *raiz, int tipoImp)
+{
+	if(raiz != NULL){
+		switch(tipoImp){
+			// EmOrdem
+			case 1:
+				imprimirPercurso(raiz->esq,tipoImp);
+				printf("%d ",raiz->conteudo);
+				imprimirPercurso(raiz->dir,tipoImp);
+				break;
+			// PreOrdem
+			case 2:
+				printf("%d ",raiz->conteudo);
+				imprimirPercurso(raiz->esq,tipoImp);
+				imprimirPercurso(raiz->dir,tipoImp);
+				break;
+			// PosOrdem
+			case 3:
+				imprimirPercurso(raiz->esq,tipoImp);
+				imprimirPercurso(raiz->dir,tipoImp);
+				printf("%d ",raiz->conteudo);
+				break;
+			case 4:
+				imprimir(raiz, 1);
+				break;
+			default:
+				printf("\nTipo de impressão inválida.\n");
+		}
+	}
+}
 
 int main(){
 	setlocale(LC_ALL,"Portuguese");
 	
+	int op, valor, tipoImp;
 	No *raiz = NULL;
 	
+	do{
+		printf("\n\n\t1 - Inserir\n\t2 - Remover\n\t3 - Imprimir\n\t0 - Sair\n\n");
+		scanf("%d",&op);
+		
+		switch(op){
+			case 0:
+				printf("\nPrograma encerrado...");
+				break;
+			case 1:
+				printf("\n\tDigite o valor a ser inserido: ");
+				scanf("%d",&valor);
+				raiz = inserir(raiz, valor);
+				imprimir(raiz, 1);
+				break;
+			case 2:
+				printf("\n\tDigite o valor a ser removido: ");
+				scanf("%d",&valor);
+				raiz = remover(raiz, valor);
+				printf("\n");
+				imprimir(raiz, 1);
+				break;
+			case 3:
+				printf("\n");
+				printf("\n\n\tEscolha um tipo de impressão\n\t1 - InOrdem\n\t2 - PreOrdem\n\t3 - PosOrdem\n\t4 - Árvore\n\n");
+				scanf("%d",&tipoImp);
+				printf("\n\tImpressão %s\n\t",tipoImp == 1 ? "InOrdem":tipoImp == 2? "PreOrdem":"PosOrdem");
+				imprimirPercurso(raiz, tipoImp);
+				printf("\n");
+				break;
+			default:
+				printf("\nOpção inválida!\n");
+		}
+		
+	}while(op != 0);
 	
 	return 0;
 }
